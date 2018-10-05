d4a038a1e7935984ee91f50615b86ee39924b2b2
Alok Tiwari <aloktiwari@Aloks-Binary-MacBook-Pro.local>
2018-10-05 17:19:02 +0800

    added the config file

---
+++ config.py
@@ -0,0 +1,29 @@
+LOGFILE = '/home/workgroup/alok/logs/fetch_transactions.log'
+QUERY_EVERY_N_SECONDS = 15
+COL_NAMES = ['id',
+                'purchase_time',
+                'underlying_symbol',
+                'payout_price',
+                'buy_price',
+                'sell_price',
+                'start_time',
+                'expiry_time',
+                'delay',
+                'duration',
+                'bet_type',
+                'remark',
+                'short_code',
+                'sell_time',
+                'client_loginid',
+                'staff_loginid',
+                'market',
+                'submarket',
+                'currency_code',
+                'residence',
+                'date_joined',
+                'tick_count',
+                'myaffiliates_token',
+                'source']
+CREDENTIALS = 'db_creds.pkl'
+QUERY = 'query_nowhere.sql'
+MIN_RECORDS = 50
